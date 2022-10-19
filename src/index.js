ee6407861674c1c57ecb019f7d2055d3d1fc3da9
Michael <michael.kononenko101@gmail.com>
2022-10-19 23:39:22 +0300

    add few changes after mentors check

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import './css/styles.css';
 import {
   countryList,
   countryInfo,
   fetchCountries,
 } from './js/fetchCountries.js';
+import { renderOneCountry, renderCountriesList } from './js/rendering.js';
 const DEBOUNCE_DELAY = 300;
 const debounce = require('lodash.debounce');
 
@@ -17,5 +19,20 @@ function inputHandling(event) {
     countryInfo.innerHTML = '';
     return;
   }
-  fetchCountries(name);
+
+  fetchCountries(name)
+    .then(data => {
+      if (data.length > 10) {
+        Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+      } else if (data.length >= 2 && data.length <= 10) {
+        renderCountriesList(data);
+      } else if (data.length === 1) {
+        renderOneCountry(data[0]);
+      }
+    })
+    .catch(() => {
+      Notify.failure('Oops, there is no country with that name');
+    });
 }
